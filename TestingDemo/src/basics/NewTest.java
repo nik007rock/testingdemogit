380dbab5ac1a96bb9828fcfbe48c14b116c5131c
nikhil <nikhil.kalwaghe@gmail.com>
2019-09-25 21:10:54 +0530

    added testng code

---
+++ TestingDemo/src/basics/NewTest.java
@@ -0,0 +1,16 @@
+package basics;
+
+import org.testng.annotations.Test;
+
+public class NewTest {
+  @Test
+  public void f()
+  {
+	  System.out.println("First");
+  }
+  @Test
+  public void f1() 
+  {
+	  System.out.println("Second");
+  }
+}
